dfe3af910ed7bb6ecc474b5849263301b8ff53a4
Luke Zhao <me@lkzhao.com>
2023-06-19 10:27:55 -0700

    Better ShapeView

--- Sources/BaseView/UtilityViews/GradientView.swift
+++ Sources/BaseView/UtilityViews/GradientView.swift
@@ -1,45 +1,45 @@
 import UIKit
 
 open class GradientView: View {
-  open override class var layerClass: AnyClass {
-    return CAGradientLayer.self
-  }
-
-  open var gradientLayer: CAGradientLayer {
-    return layer as! CAGradientLayer
-  }
-
-  open var colors: [UIColor] = [] {
-    didSet {
-      guard colors != oldValue else { return }
-      updateColors()
+    open override class var layerClass: AnyClass {
+        return CAGradientLayer.self
     }
-  }
-
-  open var locations: [CGFloat] = [] {
-    didSet {
-      gradientLayer.locations = locations as [NSNumber]
+    
+    open var gradientLayer: CAGradientLayer {
+        return layer as! CAGradientLayer
     }
-  }
-
-  open var startPoint: CGPoint {
-    get { return gradientLayer.startPoint }
-    set { gradientLayer.startPoint = newValue }
-  }
-
-  open var endPoint: CGPoint {
-    get { return gradientLayer.endPoint }
-    set { gradientLayer.endPoint = newValue }
-  }
-
-  open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-    super.traitCollectionDidChange(previousTraitCollection)
-    updateColors()
-  }
-
-  private func updateColors() {
-    gradientLayer.colors = colors.map {
-      $0.resolvedColor(with: traitCollection).cgColor
+    
+    open var colors: [UIColor] = [] {
+        didSet {
+            guard colors != oldValue else { return }
+            updateColors()
+        }
     }
-  }
+    
+    open var locations: [CGFloat] = [] {
+        didSet {
+            gradientLayer.locations = locations as [NSNumber]
+        }
+    }
+    
+    open var startPoint: CGPoint {
+        get { return gradientLayer.startPoint }
+        set { gradientLayer.startPoint = newValue }
+    }
+    
+    open var endPoint: CGPoint {
+        get { return gradientLayer.endPoint }
+        set { gradientLayer.endPoint = newValue }
+    }
+    
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateColors()
+    }
+    
+    private func updateColors() {
+        gradientLayer.colors = colors.map {
+            $0.resolvedColor(with: traitCollection).cgColor
+        }
+    }
 }
--- Sources/BaseView/UtilityViews/ShapeView.swift
+++ Sources/BaseView/UtilityViews/ShapeView.swift
@@ -1,45 +1,66 @@
-//
-//  ShapeView.swift
-//  shuffles
-//
-//  Created by Luke Zhao on 8/4/21.
-//
-
 import Foundation
 import UIKit
+import BaseToolbox
 
 open class ShapeView: View {
-  public override class var layerClass: AnyClass {
-    return CAShapeLayer.self
-  }
+    public override class var layerClass: AnyClass {
+        CAShapeLayer.self
+    }
+    
+    public var shapeLayer: CAShapeLayer {
+        layer as! CAShapeLayer
+    }
+    
+    public var path: UIBezierPath? {
+        didSet {
+            shapeLayer.path = path?.cgPath
+        }
+    }
+    
+    public var fillColor: UIColor? {
+        didSet {
+            shapeLayer.fillColor = fillColor?.cgColor
+        }
+    }
+    
+    public var strokeColor: UIColor? {
+        didSet {
+            shapeLayer.strokeColor = strokeColor?.cgColor
+        }
+    }
 
-  public var shapeLayer: CAShapeLayer {
-    return layer as! CAShapeLayer
-  }
+    @Proxy(\.shapeLayer.fillRule)
+    public var fillRule: CAShapeLayerFillRule
 
-  public var path: UIBezierPath? {
-    didSet {
-      shapeLayer.path = path?.cgPath
-    }
-  }
+    @Proxy(\.shapeLayer.strokeStart)
+    public var strokeStart: CGFloat
 
-  public var fillColor: UIColor? {
-    didSet {
-      shapeLayer.fillColor = fillColor?.cgColor
+    @Proxy(\.shapeLayer.strokeEnd)
+    public var strokeEnd: CGFloat
+
+    @Proxy(\.shapeLayer.lineWidth)
+    public var lineWidth: CGFloat
+
+    @Proxy(\.shapeLayer.miterLimit)
+    public var miterLimit: CGFloat
+
+    @Proxy(\.shapeLayer.lineCap)
+    public var lineCap: CAShapeLayerLineCap
+
+    @Proxy(\.shapeLayer.lineJoin)
+    public var lineJoin: CAShapeLayerLineJoin
+
+    @Proxy(\.shapeLayer.lineDashPhase)
+    public var lineDashPhase: CGFloat
+
+    @Proxy(\.shapeLayer.lineDashPattern)
+    public var lineDashPattern: [NSNumber]?
+
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
+            shapeLayer.fillColor = fillColor?.cgColor
+            shapeLayer.strokeColor = strokeColor?.cgColor
+        }
     }
-  }
-  
-  public var strokeColor: UIColor? {
-    didSet {
-      shapeLayer.strokeColor = strokeColor?.cgColor
-    }
-  }
-  
-  open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-    super.traitCollectionDidChange(previousTraitCollection)
-    if traitCollection.userInterfaceStyle != previousTraitCollection?.userInterfaceStyle {
-      shapeLayer.fillColor = fillColor?.cgColor
-      shapeLayer.strokeColor = strokeColor?.cgColor
-    }
-  }
 }
